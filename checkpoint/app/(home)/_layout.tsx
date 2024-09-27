ad2c954df165c203a945d439a24675bbe7b6c37e
sugaprivate <arinsuga@gmail.com>
2024-09-27 17:11:49 +0700

    dev1.0 - refactor: wrk

---
+++ checkpoint/app/(home)/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="login" options={{
+        headerShown: false
+      }} />
+      <Stack.Screen name="index" />
+      <Stack.Screen name="dashboard" />
+      <Stack.Screen name="history" />
+      <Stack.Screen name="historydetail" />
+      <Stack.Screen name="historysearch" />
+      <Stack.Screen name="checkin" />
+      <Stack.Screen name="checkout" />
+    </Stack>
+  );
+}
---
+++ checkpoint/components/hdp/Logo.tsx
@@ -0,0 +1,53 @@
+import { Image, StyleSheet } from 'react-native'
+import React from 'react'
+
+
+type propType = {
+    size: string,
+}
+
+type typeSize = {
+    width?: number,
+    height?: number,
+
+};
+
+const Logo = ({ size }: propType) => {
+
+    return (
+
+        <Image
+        style={ styles[size] }
+        source={require('../assets/images/logo.png')}
+        />
+
+    )
+}
+
+const styles: { [key: string]: typeSize } = StyleSheet.create({
+
+    xs: {
+        width: 50,
+        height: 50,
+    },
+    s: {
+        width: 100,
+        height: 100,
+    },
+    m: {
+        width: 300,
+        height: 300,
+    },
+    l: {
+        width: 600,
+        height: 600,
+    },
+    xl: {
+        width: 700,
+        height: 700,
+    },
+});
+
+
+
+export default Logo
