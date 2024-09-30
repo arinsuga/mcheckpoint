4affff05bbd73471fe0a94837b2c0a807e8383d8
sugaprivate <arinsuga@gmail.com>
2024-09-30 12:08:43 +0700

    dev1.0 - prepare module checkpoint

---
+++ checkpoint/app/(checkpoint)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="login" />
+      <Stack.Screen name="index" />
+      <Stack.Screen name="dashboard" />
+      <Stack.Screen name="history" />
+      <Stack.Screen name="historydetail" />
+      <Stack.Screen name="historysearch" />
+      <Stack.Screen name="checkin" />
+      <Stack.Screen name="checkout" />
+    </Stack>
+  );
+}
---
+++ checkpoint/app/(checkpoint)/login.tsx
@@ -0,0 +1,140 @@
+
+import {
+  Button,
+  Image,
+  ImageBackground,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
+
+import { Colors } from "../constants/Colors";
+import Logo from "../components/Logo";
+
+export default function Login() {
+
+  console.log(StatusBar);
+
+  return (
+    <SafeAreaView
+      style={ [ styles.rootContainer, { backgroundColor: Colors.whiteLight } ] }
+    >
+
+      <View style={{
+        flex: 1,
+        alignItems: "center",
+        position: 'absolute',
+        top: 50,
+        alignSelf: "center",
+        width: '100%'
+      }}>
+        <Logo size="s" />
+      </View> 
+
+      <View style={ styles.container }>
+
+
+      <View style={ styles.textInputGroup } >
+
+          <Image style={ styles.inputIcon } source={require('../assets/images/user.png')}  />
+          <TextInput
+            placeholderTextColor={ Colors.grey }
+            placeholder="Username"
+            style={ styles.textInput }
+          ></TextInput>
+
+        </View>
+
+        <View style={ [ styles.textInputGroup, { marginBottom: 30 } ] }>
+
+          <Image style={ styles.inputIcon } source={require('../assets/images/key.png')} />
+          <TextInput
+            placeholderTextColor={ Colors.grey }
+            secureTextEntry={ true }
+            placeholder="Password"
+            style={ styles.textInput }
+          ></TextInput>
+
+        </View>
+
+        <View>
+
+          <TouchableOpacity style={ [styles.btn, styles.btnLogin] }>
+            <Text style={ styles.btnText }>Login</Text>
+          </TouchableOpacity>
+
+        </View>
+
+      </View>
+
+
+
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  rootContainer: {
+
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+
+  },
+
+  container: {
+    paddingBottom: 50,
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+
+  btn: {
+    alignItems: 'center',
+    padding: 15,
+    color: Colors.white,
+    borderRadius: 5,
+
+  },
+
+  btnLogin: {
+    
+    backgroundColor: Colors.bgOrange,
+
+  },
+
+  btnText: {
+    color: Colors.white,
+  },
+
+  
+  inputIcon: {
+
+    position: "absolute",
+    bottom: 10
+
+  },
+
+  textInputGroup: {
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.greyLight,
+    marginBottom: 5,
+    width: '75%'
+  },
+
+  textInput: {
+
+    fontSize: 17,
+    color: Colors.black,
+    paddingLeft: 30,
+    paddingTop: 30,
+
+  },
+
+});
