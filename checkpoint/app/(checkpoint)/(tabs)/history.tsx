529591696190f8191b5f0594f3ce09e6076b0aa6
mirza <sugamirza2@gmail.com>
2024-11-14 13:36:14 +0700

    feat/checkin-history: - add component DateList - add component TimelineList - implement history checkin/checkout

--- checkpoint/app/(checkpoint)/(tabs)/history.tsx
+++ checkpoint/app/(checkpoint)/(tabs)/history.tsx
@@ -1,19 +1,43 @@
 import { Text, View } from "react-native";
 
 import { Link } from "expo-router";
+import DateList from "@/components/DateList/DateList";
+import TimelineList from "@/components/TimelineList/TimelineList";
 
 export default function History() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        // padding: 12,
+        backgroundColor: '#eef1f5',
+        rowGap: 12,
       }}
     >
+      {/* SECTION HEADER */}
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 12}}>
+        <View style={{flexDirection: 'row', alignItems: 'center', columnGap: 8}}>
+          <Text style={{fontSize: 44, fontWeight: 'bold'}}>24</Text>
+          <View style={{flexDirection:'column'}}>
+            <Text style={{color: '#BCC1CD', fontWeight: 'bold'}}>Wed</Text>
+            <Text style={{color: '#BCC1CD', fontWeight: 'bold'}}>Jan 2020</Text>
+          </View>
+        </View>
+        <Text style={{fontWeight: 'bold', color: '#D16224', fontSize: 16}}>Today</Text>
+      </View>
+      {/* SECTION HEADER */}
 
-        <Text>HISTORY</Text>
-
+      {/* DATE LIST FILTER */}
+      <DateList/>
+      {/* DATE LIST FILTER */}
+      
+      {/* DIVIDER */}
+      <View style={{backgroundColor: 'white', height: 1}}></View>
+      {/* DIVIDER */}
+      
+      <View style={{paddingHorizontal: 12}}>
+        <TimelineList />
+      </View>
     </View>
   );
 }
