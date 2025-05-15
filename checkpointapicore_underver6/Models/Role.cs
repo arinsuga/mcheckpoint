5d25ccf5c59bf000d6959d372c07529e405873a4
sugaprivate <arinsuga@gmail.com>
2025-05-15 13:49:11 +0700

    dev - clean unused file and directory: done

--- checkpointapicore_underver6/Models/Role.cs
+++
@@ -1,9 +0,0 @@
-namespace checkpointapicore.Models
-{
-    public class Role
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<User> Users { get; set; }
-    }
-}
