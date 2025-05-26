2bcff3af66253514d7f9d2e4cf6ace7115dac2eb
sugaprivate <arinsuga@gmail.com>
2025-05-26 15:54:41 +0700

    v0.0.1-beta - Bugs fix Create PDF result failed: done example: - checkin data : 19/05/2025, 23/05/2025, 23/05/2025 - checkout data : 26/05/2025, 26/05/2025, 26/05/2025
    skenario 1 failed [no result]
    - period: 25/05/2025 to 26/05/2025 = no result (must 3 results)
    
    skenario 2 success [result ok]
    - period: 19/05/2025 to 26/05/2025 = 3 results ok
    
    fix: skenario 1 must success

--- checkpointapi/arins/Fo/Repositories/Attend/AttendRepository.php
+++ checkpointapi/arins/Fo/Repositories/Attend/AttendRepository.php
@@ -51,19 +51,27 @@ class AttendRepository extends EloquentRepository implements AttendRepositoryInt
 
         if (isset($parCheckpoint_dt1) && isset($parCheckpoint_dt2)) {
 
-            if ($parCheckpoint_dt1 == $parCheckpoint_dt2) {
+            // if ($parCheckpoint_dt1 == $parCheckpoint_dt2) {
 
-                $result = $result->where($checkin_time, '>=', $parCheckpoint_dt1);
-                $result = $result->orWhere($checkout_time, '>=', $parCheckpoint_dt1);
+            //     $result = $result->where($checkin_time, '>=', $parCheckpoint_dt1);
+            //     $result = $result->orWhere($checkout_time, '>=', $parCheckpoint_dt1);
 
-            } //end if
 
-            if ($parCheckpoint_dt1 < $parCheckpoint_dt2) {
-                $result = $result->whereBetween($checkin_time, [$parCheckpoint_dt1, $parCheckpoint_dt2]);
-                $result = $result->orWhereBetween($checkout_time, [$parCheckpoint_dt1, $parCheckpoint_dt2]);
-            }
+            // } //end if
+
+            // if ($parCheckpoint_dt1 < $parCheckpoint_dt2) {
+
+            //     $result = $result->whereBetween($checkin_time, [$parCheckpoint_dt1, $parCheckpoint_dt2]);
+            //     $result = $result->orWhereBetween($checkout_time, [$parCheckpoint_dt1, $parCheckpoint_dt2]);
+
+            // }
+
+            $result = $result->whereRaw("(DATE(checkin_time) >= ? and DATE(checkin_time) <= ?) or (DATE(checkout_time) >= ? and DATE(checkout_time) <= ?)",
+            [$parCheckpoint_dt1->toDateString(), $parCheckpoint_dt2->toDateString(), $parCheckpoint_dt1->toDateString(), $parCheckpoint_dt2->toDateString()]);
             
         } //end if
+
+        // return $result->toSql();
 
         return $result->get();
     }
