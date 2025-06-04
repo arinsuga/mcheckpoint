ecada687aa392cbe03b0173c33f219e12f81a883
sugaprivate <arinsuga@gmail.com>
2025-06-04 13:15:34 +0700

    v1.1.0 - upgrade uiux home screen: wrk - Tambah Floating button ( Checkin/Checkout ) - Tambahkan button create PDF - Tampilkan List Summary Chekin/Checkout sesuai tanggal hari ini - Tampilkan detail Checkin/Checkout jika List Summary di tap

--- checkpoint/app/(checkpoint)/(tabs)/index.tsx
+++ checkpoint/app/(checkpoint)/(tabs)/index.tsx
@@ -1,40 +1,123 @@
-import React, { useState } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+  StyleSheet,
+  Dimensions,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+
+//Packages
+import { useRouter, Router } from "expo-router";
 
 //Components
 import Logo from "@/components/Logo/Logo";
 import Icon from "@/components/Icon/Icon";
 
 //Services
-import { refreshAuthToken } from "@/services/AuthService";
+import { refreshAuthToken, getUsername } from "@/services/AuthService";
+import { check } from '@/services/CheckpointService';
+
+//Interfaces
+import ICheckpoint from "@/interfaces/ICheckpoint";
+
+//Constants
 import { Colors } from "@/constants/Colors";
 
 export default function Home() {
-  const [token, setToken] = useState('');
+    const [action, setAction] = useState<'checkin' | 'checkout' | ''>('');  
 
-  const handleRefreshToken = async () => {
+    const router: Router = useRouter();
+
+    const getStyles = (action: 'checkin' | 'checkout' | '') => {
+
+      const styles = StyleSheet.create({
+
+        checkButton: {
+          flex: 1,
+          position: "absolute",
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: Dimensions.get('window').width*0.3,
+          padding: 10,
+          borderRadius: 10,
+          bottom: 30,
+          right: 30,
+          backgroundColor: action == 'checkin' ? Colors.green : action == 'checkout' ? Colors.red : Colors.grey,
+        }
+
+      });
+
+      return styles
+    }
+
+    useEffect(() => {
+
+      const fetchData = async () => {
+
+        const userName = await getUsername();
+        const checkResult = await check(userName as string)
+
+        if (checkResult) {
+
+          setAction(checkResult.action);
+
+        }
+
+      }
+      fetchData();
 
 
-    const refreshToken = await refreshAuthToken();
+    }, []);
 
-    setToken(refreshToken as string);
+    return (
+      <>
+        <View style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
 
-  }
-
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-
-        <Logo size="s" />
+            <Logo size="s" />
+            </View>
+            <Text style={{ marginBottom: 40 }}>1.1.0</Text>
         </View>
-        <Text style={{ marginBottom: 40 }}>1.1.0</Text>
 
-    </View>
-  );
+              {
+
+                action == 'checkin' || action == 'checkout' ?
+                <TouchableOpacity
+                    onPress={ async() => router.push('/pinloc')} 
+                    style={ getStyles(action).checkButton }
+                >
+                  <Text style={{color: Colors.white}}>{ action == 'checkin' ? 'Checkin' : 'Checkout' }</Text>
+                </TouchableOpacity> :
+                <View
+                    style={[
+                      getStyles(action).checkButton,
+                      {
+                        paddingTop: 5,
+                        paddingBottom: 5,
+                      }
+                    ]}
+                >
+                  <ActivityIndicator  size={30} color={ Colors.white } />
+                </View>
+
+              }
+
+
+        
+      </>
+    );
 }
